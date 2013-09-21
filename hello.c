81edbae959cd8d980690458491ad2cf5e83f5c0b
David Kim <kim.david.j@gmail.com>
2013-09-21 12:38:21 -0700

    ...

---
+++ hello.c
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+	printf("Hello\n");
+	
+	int a=0;
+	int b=2;
+	while(100!=a)
+	{
+		++a;
+		b*=b;
+		cout << a << endl;
+	}
+
+	cout << "Final b: " << b << endl << '\n';
+
+	return 0;
+}
